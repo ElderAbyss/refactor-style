5250fa84e7775bc24d05162a564eda24104922ea
ElderAbyss <elderabyss@users.noreply.github.com>
2018-03-21 19:14:11 -0400

    fixed sync issues with channels

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ type cssFile struct {
 	classes  map[string][]string
 }
 
-func deepCopyCssFile(source cssFile) cssFile {
+func (source cssFile) deepCopyCssFile() cssFile {
 	extractedFile := source
 	extractedFile.classes = map[string][]string{}
 	for key, value := range source.classes {
@@ -49,32 +49,47 @@ func filterFile(baseFile cssFile, filterFile cssFile) cssFile {
 	return result
 }
 
-func reduceCSS(fileName string, parsed chan cssFile) error {
+type cssChannel struct {
+	c    chan cssFile
+	once sync.Once
+}
+
+func newCssChannel() *cssChannel {
+	return &cssChannel{c: make(chan cssFile)}
+}
+
+func (cc *cssChannel) closeSafe() {
+	cc.once.Do(func() {
+		close(cc.c)
+	})
+}
+
+func reduceCSS(fileName string, parsed *cssChannel) error {
 	var parsedCSS cssFile
 	parsedCSS.classes = make(map[string][]string)
-	cutset := "\r\n \t;"
+	cutSet := "\r\n \t;"
 	rawText, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		return fmt.Errorf("unable to read file %s - %s\n", fileName, err.Error())
 	}
-	textString := strings.Trim(string(rawText), cutset)
+	textString := strings.Trim(string(rawText), cutSet)
 	for len(textString) > 0 {
 		leftBracket := strings.Index(textString, "{")
 		rightBracket := strings.Index(textString, "}")
-		classNames := strings.Trim(textString[0:leftBracket-1], cutset)
-		classDef := strings.Trim(textString[leftBracket+1:rightBracket-1], cutset)
+		classNames := strings.Trim(textString[0:leftBracket-1], cutSet)
+		classDef := strings.Trim(textString[leftBracket+1:rightBracket-1], cutSet)
 		classAttr := strings.Split(classDef, ";")
 		for i, item := range classAttr {
-			classAttr[i] = strings.Trim(item, cutset)
+			classAttr[i] = strings.Trim(item, cutSet)
 		}
 		sort.Strings(classAttr)
 		classDef = strings.Join(classAttr, ";\n") + ";\n"
 		parsedCSS.classes[classDef] = append(parsedCSS.classes[classDef], classNames)
-		textString = strings.Trim(textString[rightBracket+1:], cutset)
+		textString = strings.Trim(textString[rightBracket+1:], cutSet)
 	}
 	parsedCSS.fileName = fileName
 	fmt.Printf("adding %s to the parsed chan\n", fileName)
-	parsed <- parsedCSS
+	parsed.c <- parsedCSS
 	return nil
 }
 
@@ -96,7 +111,7 @@ func extractCommonStyles(parsedFiles []cssFile) []cssFile {
 	var refactored cssFile
 	refactored.fileName = "refactored.css"
 	refactored.classes = map[string][]string{}
-	extractedFile := deepCopyCssFile(parsedFiles[0])
+	extractedFile := parsedFiles[0].deepCopyCssFile()
 	for key, value := range extractedFile.classes {
 		extracting := true
 		for i := 0; i < len(parsedFiles); i++ {
@@ -130,19 +145,21 @@ func main() {
 
 	// parse of each file
 	var taskWG, saveWG sync.WaitGroup
-	task := make(chan string)
-	parsed := make(chan cssFile)
-	save := make(chan cssFile)
+	task := newCssChannel()
+	parsed := newCssChannel()
+	save := newCssChannel()
 	taskWG.Add(*maxWorkers)
 	for w := 0; w < *maxWorkers; w++ {
 		go func() {
-			defer taskWG.Done()
-			for file := range task {
-				myErr := reduceCSS(file, parsed)
+			for file := range task.c {
+				myErr := reduceCSS(file.fileName, parsed)
 				if myErr != nil {
 					fmt.Print(myErr.Error())
 				}
 			}
+			taskWG.Done()
+			taskWG.Wait()
+			parsed.closeSafe()
 		}()
 	}
 
@@ -150,7 +167,7 @@ func main() {
 	for w := 0; w < *maxWorkers; w++ {
 		go func() {
 			defer saveWG.Done()
-			for cssFileData := range save {
+			for cssFileData := range save.c {
 				myErr := saveCSS(cssFileData)
 				if myErr != nil {
 					fmt.Println(myErr.Error())
@@ -160,29 +177,29 @@ func main() {
 	}
 
 	// process files
-	cssCount := 0
 	go func() {
 		for _, file := range files {
 			if strings.HasSuffix(strings.ToLower(file.Name()), ".css") {
 				fmt.Printf("%s added to target group\n", file.Name())
-				cssCount++
-				task <- *dir + "/" + file.Name()
+				targetFile := cssFile{fileName: *dir + "/" + file.Name()}
+				task.c <- targetFile
 			}
 		}
-		close(task)
+		task.closeSafe()
 	}()
+
 	//taskWG.Wait()
-	parsedFiles := make([]cssFile,0)
-	for i := 0; i < cssCount; i++ {
-		parsedFiles = append(parsedFiles, <-parsed)
+	parsedFiles := make([]cssFile, 0)
+	//for i := 0; i < cssCount; i++ {
+	for parsedFile := range parsed.c {
+		parsedFiles = append(parsedFiles, parsedFile)
 	}
-	close(parsed)
 	fmt.Println("extracting common styles")
 	processedFiles := extractCommonStyles(parsedFiles)
 	for _, completedFile := range processedFiles {
-		save <- completedFile
+		save.c <- completedFile
 	}
-	close(save)
+	save.closeSafe()
 	saveWG.Wait()
 }
 
